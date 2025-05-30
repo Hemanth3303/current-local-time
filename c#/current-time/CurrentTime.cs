4721d183c4f2686ff01062839b6d6ce8ceb8a7ba
Hemanth3303 <109304485+Hemanth3303@users.noreply.github.com>
2025-05-30 23:38:35 +0530

    c#

---
+++ c#/current-time/CurrentTime.cs
@@ -0,0 +1,11 @@
+﻿class CurrentTime
+{
+	static void Main()
+	{
+		DateTime currentTime = DateTime.Now;
+
+		Console.WriteLine("24 Hour Format: " + currentTime.ToString("yyyy-MM-dd HH:mm:ss"));
+		Console.WriteLine("12 Hour Format: " + currentTime.ToString("yyyy-MM-dd hh:mm:ss tt"));
+	}
+
+}
